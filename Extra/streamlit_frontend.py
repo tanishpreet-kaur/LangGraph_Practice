2c0d453a83d4b985c424e08aa33bd14bed6b9fd9
Tanishpreet Kaur <167232732+tanishpreet-kaur@users.noreply.github.com>
2026-04-18 22:20:24 +0530

    Add files via upload

---
+++ Extra/streamlit_frontend.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here...')
+
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+    
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]})
+    ai_message = response['messages'][-1].content
+    
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})  
+    with st.chat_message('assistant'):
+        st.text(ai_message)
